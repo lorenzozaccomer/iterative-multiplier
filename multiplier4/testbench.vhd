8a04bf4eaa15c0ce661aa3cb3440279be0b40b30
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-10-13 09:45:01 +0200

    some little changes for multiplier4

--- multiplier4/testbench.vhd
+++ multiplier4/testbench.vhd
@@ -1,43 +1,55 @@
 
 -- testbench.vhdl for multiplier
 
+-- libraries
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+
+-- interface
 entity testbench is
 end testbench;
 
-architecture s of testbench is
-  signal SIG1: std_logic_vector(3 downto 0);
-  signal SIG2: std_logic_vector(3 downto 0);
-  signal SIG_RES: std_logic_vector(7 downto 0);
-  
-  component multiplier is
-    port( 
-      A   : in std_logic_vector(3 downto 0);
-      B   : in std_logic_vector(3 downto 0);
-      R   : out std_logic_vector(7 downto 0)  );
-  end component multiplier;
+
+-- architecture
+architecture tb_multiplier of testbench is
+
+	constant CLK_SEMIPERIOD0 : time := 25ns;
+	constant CLK_SEMIPERIOD1 : time := 25ns;
+	constant CLK_PERIOD : time := CLK_SEMIPERIOD0+CLK_SEMIPERIOD1;
+	signal RST : std_logic;
+	
+	signal A_SIG: std_logic_vector(3 downto 0);
+	signal B_SIG: std_logic_vector(3 downto 0);
+	signal R_SIG: std_logic_vector(7 downto 0);
+
+	component multiplier is
+	port( 
+	  A   : in std_logic_vector(3 downto 0);
+	  B   : in std_logic_vector(3 downto 0);
+	  R   : out std_logic_vector(7 downto 0)  );
+	end component multiplier;
       
 begin
   
 	multiplier_INST : multiplier
 	port map(
-		A	=> SIG1,
-		B	=> SIG2,
-		R	=> SIG_RES	);
+		A	=> A_SIG,
+		B	=> B_SIG,
+		R	=> R_SIG	);
 	
 	process is
 	begin
-		SIG1 <= "0000";
-		SIG2 <= "0000";
+		A_SIG <= "0000";
+		B_SIG <= "0000";
 		wait for 10 ns;
-		SIG1 <= "0001";
-		SIG2 <= "0001";
+		A_SIG <= "0001";
+		B_SIG <= "0001";
 		wait for 10 ns;
-		SIG1 <= "1011";
-		SIG2 <= "0010";
+		A_SIG <= "1011";
+		B_SIG <= "0010";
 		wait for 10 ns;
 	end process;
 	
-end s;
+end tb_multiplier;

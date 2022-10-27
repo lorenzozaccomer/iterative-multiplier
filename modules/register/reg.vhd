931b1cb00885ab2a87172d7e49a1c9a4e8576568
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-10-27 10:10:18 +0200

    reset reg for 1 value, not 0 anymore

--- modules/register/reg.vhd
+++ modules/register/reg.vhd
@@ -6,10 +6,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity reg is
-	generic(
-	  N 	: integer := 4 );
+	generic(N 	: integer := 4);
 	port(
-		CLK, RST_N, LOAD : in std_logic;
+		CLK, RST, LOAD : in std_logic;
 		D : in std_logic_vector(N-1 downto 0);
 		Q : out std_logic_vector(N-1 downto 0)
 	);
@@ -17,12 +16,12 @@ end reg;
 
 architecture behavior of reg is
 	begin
-	process(CLK, RST_N)
+	process(CLK, RST)
 	begin
-		if(RST_N = '0') then
+		if(RST = '1') then
 			Q <= (others => '0');
 		elsif rising_edge(CLK) and LOAD='1' then
-			Q <= D;
+			Q <= D after 1 ns;
 		end if;
 	end process;
 end behavior;
--- modules/register/reg_testbench.vhd
+++ modules/register/reg_testbench.vhd
@@ -7,8 +7,7 @@ use ieee.numeric_std.all;
 
 -- interface
 entity testbench is
-	generic(
-	  N 	: integer := 4 );
+	generic(N 	: integer := 4);
 end testbench;
 
 -- architecture
@@ -20,13 +19,13 @@ architecture reg_testbench of testbench is
 	constant CLK_PERIOD : time := CLK_SEMIPERIOD0+CLK_SEMIPERIOD1;
 
 	-- signals for DUT
-	signal CLK, RST_N, LOAD: std_logic;
+	signal CLK, RST, LOAD: std_logic;
 	signal D, Q: std_logic_vector(N-1 downto 0);
 	
 	-- DUT declaration
 	component reg is
 	port(
-		CLK, RST_N, LOAD : in std_logic;
+		CLK, RST, LOAD : in std_logic;
 		D : in std_logic_vector(N-1 downto 0);
 		Q : out std_logic_vector(N-1 downto 0)
 	);
@@ -37,7 +36,7 @@ architecture reg_testbench of testbench is
 	DUT : reg
 	port map(
 		CLK => CLK,
-		RST_N => RST_N,
+		RST => RST,
 		LOAD => LOAD,
 		D => D,
 		Q => Q
@@ -46,25 +45,27 @@ architecture reg_testbench of testbench is
 	-- RESET
 	reset_process : process
 	begin
-		RST_N <= '1';
+		RST <= '0';
 		wait for 1 ns;
-		RST_N <= '0';
+		RST <= '1';
 		wait for 3 ns;
-		RST_N <= '1';
+		RST <= '0';
 		wait;
 	end process reset_process;
 	
-	process is
+	-- TEST
+	test_process : process
 	begin
 		D <= "1011";
 		LOAD <= '1';
 		wait for 10 ns;
-		-- D <= "0110";
-		-- LOAD <= '0';
-		-- wait for 4 ns;
-		-- D <= "0011";
-		-- LOAD <= '1';
-	end process;	
+		D <= "0110";
+		LOAD <= '1';
+		wait for 10 ns;
+		D <= "0011";
+		LOAD <= '0';
+		wait for 10 ns;
+	end process test_process;	
 	
 	-- CLOCK
 	clk_process: process

73c2cd62ca8f62c1c91afc14b0859fd3c998db27
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-10-27 11:59:22 +0200

    add shifter to components modules for iterative multiplier

--- iterative_multiplier/components.vhd
+++ iterative_multiplier/components.vhd
@@ -40,7 +40,7 @@ package component_package is
 	component shifter is
 		generic(N 	: integer := 4);
 		port( 
-			CLK, RST_N, SH_ENABLE: in std_logic;
+			CLK, RST, SH_ENABLE: in std_logic;
 			D: in std_logic_vector(N-1 downto 0);
 			Q: out std_logic_vector(N-1 downto 0)
 		);
@@ -112,7 +112,7 @@ architecture behavior of reg is
 	begin
 	process(CLK, RST)
 	begin
-		if(RST = '0') then
+		if(RST = '1') then
 			Q <= (others => '0');
 		elsif rising_edge(CLK) and LOAD='1' then
 			Q <= D after TRco;
@@ -142,5 +142,37 @@ end entity;
 architecture behavior of basic_multiplier is
   begin
     P <= std_logic_vector(unsigned(A) * unsigned(B));
+end behavior;
+-----------------------------------------------------------------
+
+
+
+-----------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;	-- needed to shift op
+use work.constants_components_package.all;
+
+entity shift is
+	generic(N 	: integer := 4);
+	port( 
+		CLK, RST, SH_ENABLE: in std_logic;
+		D: in std_logic_vector(N-1 downto 0);
+		Q: out std_logic_vector(N-1 downto 0)
+	);
+end entity;
+
+architecture behavior of shift is
+	begin
+		process(CLK, RST)
+		begin
+			if rising_edge(CLK) then
+				if RST = '1' then			-- clear
+					Q <= (others => '0');
+				elsif SH_ENABLE = '1' then	-- shift
+					Q <= std_logic_vector(shift_left(unsigned(D), 1));
+				end if;
+			end if;
+		end process;
 end behavior;
 -----------------------------------------------------------------

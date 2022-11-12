d5ba0d4c8e07a462027bf87d57e0a1b42606e7a9
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-11-12 10:24:16 +0100

    upload of datapath for bm_sel

---
+++ iterative_multiplier/bm_sel/datapath.vhd
@@ -0,0 +1,16 @@
+
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity datapath is
+	port(
+		CLK, RST: in std_logic;
+			-- data inputs
+			-- data outputs
+			-- control signals
+			-- status signals
+		);
+entity datapath;

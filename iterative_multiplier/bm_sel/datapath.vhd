40b20b98b7aaf6fd55647cd2f3f9fb59a395bb4c
lorenzozaccomer <lz.131034@gmail.com>
2022-11-17 11:57:14 +0100

    upload new version of bm_sel's datapath

--- iterative_multiplier/bm_sel/datapath.vhd
+++ iterative_multiplier/bm_sel/datapath.vhd
@@ -12,6 +12,10 @@ entity datapath is
 		M 	: integer := 4);
 	port(
 		CLK, RST:			in std_logic;
+			-- data inputs
+		RA_BM, RB_BM:		in std_logic_vector(M-1 downto 0);
+			-- data outputs
+		ROUT_BM:			out std_logic_vector(2*M-1 downto 0);
 			-- control inputs
 		CALC:				in std_logic;
 		DATAIN:				in std_logic;
@@ -41,5 +45,17 @@ entity datapath is
 end datapath;
 
 architecture struct of datapath is
-	component reg4 is
+	
+
+	-- signals
+	signal temp_in, temp_out: 	std_logic_vector(M-1 downto 0);
+	signal opa_in, opa_out:		std_logic_vector(Q-1 downto 0);
+	signal opb_in, opb_out:		std_logic_vector(Q-1 downto 0);
+	
+	begin
+	
+	TEMP_BM:	regN generic map(M) port map(CLK, RST, loadTEMP_BM, RB_BM, TEMP_BM);
+	OPA:		regN generic map(Q) port map(CLK, RST, loadOPA, opa_in, opa_out);
+	OPB:		regN generic map(Q) port map(CLK, RST, loadOPB, opb_in, opb_out);
+	OPR:		regN generic map(M) port map(CLK, RST, loadOPR, opr_in, opr_out);
 	

d995a4ad962b0e37fc8cf763714dd0451946f61f
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-11-23 18:49:06 +0100

    bugfix to mux4N components module

--- iterative_multiplier/bm_sel/components.vhd
+++ iterative_multiplier/bm_sel/components.vhd
@@ -93,10 +93,10 @@ package components_package is
 		generic(N 	: integer := 8);
 		port (
 		sel: 	in std_logic_vector(1 downto 0);
-		IN0: 	in std_logic_vector(N-1 downto 0);
-		IN1: 	in std_logic_vector(N-1 downto 0);
-		IN2: 	in std_logic_vector(N-1 downto 0);
-		IN3: 	in std_logic_vector(N-1 downto 0);
+		I0: 	in std_logic_vector(N-1 downto 0);
+		I1: 	in std_logic_vector(N-1 downto 0);
+		I2: 	in std_logic_vector(N-1 downto 0);
+		I3: 	in std_logic_vector(N-1 downto 0);
 		Y:		out std_logic_vector(N-1 downto 0)
 		);
 	end component;

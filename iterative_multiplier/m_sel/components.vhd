4f7871c1f8b814598d4596280e1b2dabe6e75c62
Lorenzo Zaccomer <lz.131034@gmail.com>
2022-12-29 21:34:26 +0100

    remove not used modules from M_SEL's components

--- iterative_multiplier/m_sel/components.vhd
+++ iterative_multiplier/m_sel/components.vhd
@@ -48,16 +48,6 @@ package msel_components_package is
 	end component;
 	
 	
-	component mux is
-		port (
-		sel: 	in std_logic;
-		I0: 	in std_logic;
-		I1: 	in std_logic;
-		Y:		out std_logic
-		);
-	end component;
-
-
 	component mux2N is
 		generic(N 	: integer := 8);
 		port (
@@ -68,30 +58,7 @@ package msel_components_package is
 		);
 	end component;
 	
-	
-	component mux4N is
-		generic(N 	: integer := 8);
-		port (
-		sel: 	in std_logic_vector(1 downto 0);
-		I0: 	in std_logic_vector(N-1 downto 0);
-		I1: 	in std_logic_vector(N-1 downto 0);
-		I2: 	in std_logic_vector(N-1 downto 0);
-		I3: 	in std_logic_vector(N-1 downto 0);
-		Y:		out std_logic_vector(N-1 downto 0)
-		);
-	end component;
-	
-	
-	component reg is
-		port(
-		CLK:	in std_logic;
-		RST:	in std_logic;
-		LOAD:	in std_logic;
-		D : 	in std_logic;
-		Q : 	out std_logic
-		);
-	end component;
-	
+			
 end msel_components_package;
 ----------------------------------------------------------------------
 
@@ -134,29 +101,6 @@ end behavior;
 ----------------------------------------------------------------------
 
 
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.constants_components_package.all;
-
-
-entity mux is
-	port (
-	sel: 	in std_logic;
-	I0: 	in std_logic;
-	I1: 	in std_logic;
-	Y:		out std_logic
-	);
-end mux;
-
-architecture behavior of mux is
-begin
-	with sel select
-		Y <= 	I0 when '0', 
-				I1 when others;
-end behavior;
-----------------------------------------------------------------------
 
 ----------------------------------------------------------------------
 library ieee;
@@ -180,36 +124,6 @@ begin
 	with sel select
 		Y <= 	I0 when '0', 
 				I1 when others;
-end behavior;
-----------------------------------------------------------------------
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.constants_components_package.all;
-
-
-entity mux4N is
-	generic(N 	: integer := 8);
-	port (
-	sel: 	in std_logic_vector(1 downto 0);
-	I0: 	in std_logic_vector(N-1 downto 0);
-	I1: 	in std_logic_vector(N-1 downto 0);
-	I2: 	in std_logic_vector(N-1 downto 0);
-	I3: 	in std_logic_vector(N-1 downto 0);
-	Y:		out std_logic_vector(N-1 downto 0)
-	);
-end mux4N;
-
-architecture behavior of mux4N is
-begin
-	with sel select
-        Y <= I0 when "00",
-             I1 when "01",
-             I2 when "10",
-             I3 when others;
 end behavior;
 ----------------------------------------------------------------------
 
@@ -287,39 +201,6 @@ architecture behavior of regN is
 	begin
 		if(RST = '1') then
 			Q <= (others => '0');
-		elsif rising_edge(CLK) and LOAD='1' then
-			Q <= D;
-		end if;
-	end process;
-end behavior;
-
-
-----------------------------------------------------------------------
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.constants_components_package.all;
-
-
-entity reg is
-	port(
-		CLK:	in std_logic;
-		RST:	in std_logic;
-		LOAD:	in std_logic;
-		D : 	in std_logic;
-		Q : 	out std_logic
-	);
-end reg;
-
-architecture behavior of reg is
-	begin
-	process(CLK, RST)
-	begin
-		if(RST = '1') then
-			Q <= '0';
 		elsif rising_edge(CLK) and LOAD='1' then
 			Q <= D;
 		end if;

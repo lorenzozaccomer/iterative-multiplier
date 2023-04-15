3828a4918d8ffd08ee637f58900d6f390e7046c4
Lorenzo Zaccomer <lz.131034@gmail.com>
2023-04-16 01:24:19 +0200

    selRS become a 2 bit signal

--- iterative_multiplier/res/ctrlunit.vhd
+++ iterative_multiplier/res/ctrlunit.vhd
@@ -37,7 +37,7 @@ package res_ctrlunit_package is
 			loadS1:			out std_logic;
 			selS1:			out std_logic;
 			loadRS:			out std_logic;
-			selRS:			out std_logic;
+			selRS:			out std_logic_vector(1 downto 0);
 			loadS2:			out std_logic;
 			selS2:			out std_logic;
 			loadACCR:		out std_logic;
@@ -83,7 +83,7 @@ entity res_ctrlunit is
 			loadS1:			out std_logic;
 			selS1:			out std_logic;
 			loadRS:			out std_logic;
-			selRS:			out std_logic;
+			selRS:			out std_logic_vector(1 downto 0);
 			loadS2:			out std_logic;
 			selS2:			out std_logic;
 			loadACCR:		out std_logic;
@@ -234,10 +234,12 @@ architecture behavior of res_ctrlunit is
 							
 		loadRS			<=	'1' when state=ACC1 else
 							'0';
-		selRS			<=	'0' when state=SHIFT1 or
+		selRS			<=	"01" when state=SHIFT1 or
 								state=SHIFT2 else
-							'1' when state=SUM1 or 
-								state=SUM2;
+							"10" when state=SUM1 or 
+								state=SUM2 else
+							"11" when state=ACC1 else
+							"00";
 							
 		loadS2			<=	'1' when state=SUM2 else
 							'0';

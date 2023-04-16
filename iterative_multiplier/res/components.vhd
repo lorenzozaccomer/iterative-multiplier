59a6369b3ff3d640031ad40a01269d7947ca9641
Lorenzo Zaccomer <lz.131034@gmail.com>
2023-04-16 16:13:27 +0200

    (RES MODULE): add mux4N properly on components.vhd file

--- iterative_multiplier/res/components.vhd
+++ iterative_multiplier/res/components.vhd
@@ -182,6 +182,36 @@ end behavior;
 ----------------------------------------------------------------------
 
 
+----------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.constants_components_package.all;
+
+
+entity mux4N is
+	generic(N 	: integer := 8);
+	port (
+	sel: 	in std_logic_vector(1 downto 0);
+	I0: 	in std_logic_vector(N-1 downto 0);
+	I1: 	in std_logic_vector(N-1 downto 0);
+	I2: 	in std_logic_vector(N-1 downto 0);
+	I3: 	in std_logic_vector(N-1 downto 0);
+	Y:		out std_logic_vector(N-1 downto 0)
+	);
+end mux4N;
+
+architecture behavior of mux4N is
+begin
+	with sel select
+        Y <= I0 when "00",
+             I1 when "01",
+             I2 when "10",
+             I3 when others;
+end behavior;
+----------------------------------------------------------------------
+
+
 -- ----------------------------------------------------------------------
 -- library ieee;
 -- use ieee.std_logic_1164.all;

16b7f889d4f63bf5d66f08be1fd8c027cbf631bf
Lorenzo Zaccomer <lz.131034@gmail.com>
2023-04-28 14:33:11 +0200

    (IT_MULT16): update datapath with some elements for basic multiplier module

--- iterative_multiplier/it_mult16/datapath.vhd
+++ iterative_multiplier/it_mult16/datapath.vhd
@@ -122,6 +122,8 @@ architecture struct of it_mult16_datapath is
 	signal opa_in, opa_out:					std_logic_vector(P-1 downto 0) := (others=>'0');
 	signal opb_in, opb_out:					std_logic_vector(P-1 downto 0) := (others=>'0');
 	
+	signal bm_in, bm_out:					std_logic_vector(M-1 downto 0) := (others=>'0');
+	
 	signal a16_in, a16_out:					std_logic_vector(N-1 downto 0) := (others=>'0');
 	signal b16_in, b16_out:					std_logic_vector(N-1 downto 0) := (others=>'0');
 	signal out16_in, out_16_out:			std_logic_vector(2*N-1 downto 0) := (others=>'0');
@@ -138,23 +140,25 @@ architecture struct of it_mult16_datapath is
 	REG_EN2:	reg port map(CLK, RST, loadEN2, enable2, datain2);
 	REG_EN3:	reg port map(CLK, RST, loadEN3, enable3, datain3);
 	
-	REG_A4:		regN generic map(P) port map(CLK, RST, loadA, opa_in, opa_out);
-	REG_B4:		regN generic map(P) port map(CLK, RST, loadB, opb_in, opb_out);
+	REG_OPA:	regN generic map(P) port map(CLK, RST, loadA, opa_in, opa_out);
+	REG_OPB:	regN generic map(P) port map(CLK, RST, loadB, opb_in, opb_out);
+	
+	REG_OUTBM:	regN generic map(M) port map(CLK, RST, loadOUTBM, bm_in, bm_out);
 	
 	REG_A16:	regN generic map(N) port map(CLK, RST, loadA, a16_in, a16_out);
 	REG_B16:	regN generic map(N) port map(CLK, RST, loadB, b16_in, b16_out);
 	REG_OUT16:	regN generic map(2*N) port map(CLK, RST, loadOUT16, out16_in, out_16_out);
 	
 		-- MUXS
-	MUX_EN1:	mux2 port map(selEN1, '0', '1', enable1);
-	MUX_EN2:	mux2 port map(selEN2, '0', '1', enable2);
-	MUX_EN3:	mux2 port map(selEN3, '0', '1', enable3);
-	
-	MUX_BM1:	mux2N generic map(1) port map(selA, A, opa_out, opa_in);
-	MUX_RES1:	mux2N generic map(1) port map(selA, A, opa_out, opa_in);
-	
+	MUX_EN1:	mux2 port map(selEN1, zero, one, enable1);
+	MUX_EN2:	mux2 port map(selEN2, zero, one, enable2);
+	MUX_EN3:	mux2 port map(selEN3, zero, one, enable3);
+		
 	MUX_OPA:	mux2N generic map(P) port map(selOPA, opa_out, a_sel, opa_in);
 	MUX_OPB:	mux2N generic map(P) port map(selOPB, opb_out, b_sel, opb_in);
+	
+	MUX_OUTBM:	mux2N generic map(M) port map(selOUTBM, opb_out, b_sel, opb_in);
+	
 	
 	MUX_A16:	mux2N generic map(N) port map(selA, A, a16_out, a16_in);
 	MUX_B16:	mux2N generic map(N) port map(selB, B, b16_out, b16_in);

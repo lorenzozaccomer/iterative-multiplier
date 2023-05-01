271e931e237d5bb29741f652822329c2343f5589
Lorenzo Zaccomer <lz.131034@gmail.com>
2023-05-01 12:32:46 +0200

    update standard multiplier

--- standard_multiplier/standard_multiplier.vhd
+++ standard_multiplier/standard_multiplier.vhd
@@ -9,13 +9,14 @@ entity basic_multiplier is
 	generic(
 	  N 	: integer := 16);
 	port(
-      A, B	: in std_logic_vector(N-1 downto 0); 	-- operands
-      P		: out std_logic_vector(2*N-1 downto 0) 	-- final result
+      A			: in std_logic_vector(N-1 downto 0); 	-- operand
+      B			: in std_logic_vector(N-1 downto 0); 	-- operand
+      P			: out std_logic_vector(2*N-1 downto 0) 	-- final result
 	  );
 end entity;
 
 
-architecture behavior of basic_multiplier is
+architecture struct of basic_multiplier is
   begin
-    P <= std_logic_vector(unsigned(A) * unsigned(B));
-end behavior;
+		P <= std_logic_vector(unsigned(A) * unsigned(B));
+end struct;
--- standard_multiplier/testbench.vhd
+++ standard_multiplier/testbench.vhd
@@ -5,55 +5,118 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
 
 
 -- interface
 entity tb is
-	generic(N 	: integer := 16);
+	generic(
+		M	: integer := 8;
+		N 	: integer := 16
+	);
 end tb;
 
 
 -- architecture
-architecture tb_standard_multiplier of tb is
+architecture behavior of tb is
+
+	-- constant CLK_SEMIPERIOD0: 	time := 25 ns;
+	-- constant CLK_SEMIPERIOD1: 	time := 25 ns;
+	-- constant CLK_PERIOD: 		time := CLK_SEMIPERIOD0+CLK_SEMIPERIOD1;
+	-- constant RESET_TIME:		time := 3*CLK_PERIOD + 9 ns;
 	
-	signal A_SIG: std_logic_vector(N-1 downto 0);
-	signal B_SIG: std_logic_vector(N-1 downto 0);
-	signal R_SIG: std_logic_vector(2*N-1 downto 0);
+		-- signals for debugging and tb control
+	signal count: 				std_logic_vector(2*M-1 downto 0) := (others=> '0');
+	signal int_count: 			integer := 0;
+	signal start: 				integer := 0;
+	signal done: 				integer := 0;
+	signal counter_data: 		std_logic_vector(2*M-1 downto 0) := (others=> '0');
+	signal int_counter_data: 	integer := 0;
+	
+	signal DATAIN:				std_logic	:= '0';
+	signal DATAOUT:				std_logic	:= '0';
+		
+	signal A: std_logic_vector(N-1 downto 0);
+	signal B: std_logic_vector(N-1 downto 0);
+	signal P: std_logic_vector(2*N-1 downto 0);
 
 	component basic_multiplier is
-	port( 
-	  A   : in std_logic_vector(N-1 downto 0);
-	  B   : in std_logic_vector(N-1 downto 0);
-	  P   : out std_logic_vector(2*N-1 downto 0)
-	  );
-	end component basic_multiplier;
+		generic(
+		  N 	: integer := 16);
+		port( 
+		  A   		: in std_logic_vector(N-1 downto 0);
+		  B   		: in std_logic_vector(N-1 downto 0);
+		  P   		: out std_logic_vector(2*N-1 downto 0)
+		  );
+	end component;
       
-begin
-  
-	basic_multiplier_INST : basic_multiplier
-	port map(
-		A	=> A_SIG,
-		B	=> B_SIG,
-		P	=> R_SIG
-		);
-	
-	process is
 	begin
-			A_SIG <= "1010101010101010"; -- 16 bit
-			B_SIG <= "1101010111010101"; -- 16 bit
-		wait for 100 ns;
-			A_SIG <= "0010100001100001"; -- 16 bit
-			B_SIG <= "1000011100101001"; -- 16 bit
-		wait for 100 ns;
-			A_SIG <= "1010101010101010"; -- 16 bit
-			B_SIG <= "0000000000001101"; -- 16 bit
-		wait for 100 ns;
-			A_SIG <= "1010101010101010"; -- 16 bit
-			B_SIG <= "0000000000000101"; -- 16 bit
-		wait for 100 ns;
-			A_SIG <= "1010101010101010"; -- 16 bit
-			B_SIG <= "1101000000000000"; -- 16 bit
-		wait for 100 ns;
+  
+	DUT : basic_multiplier
+	port map(
+		A, 
+		B, 
+		P
+	);
+	
+	
+	-- read from datafile
+	read_process: process is
+		file infile: 			TEXT open READ_MODE is 
+		"C:\Users\lorenzo uni\Desktop\repositories\calcolatori-elettronici\standard_multiplier\inputdata.txt";
+		variable inputline: 	LINE;
+		variable in_A:			bit_vector(A'range);
+		variable in_B:			bit_vector(B'range);
+		variable in_DATAIN: 	bit;
+		begin
+			if not endfile(infile) then
+				readline(infile, inputline);
+				read(inputline, in_A); A <= to_UX01(in_A);
+				readline(infile, inputline);
+				read(inputline, in_B); B <= to_UX01(in_B);
+				readline(infile, inputline);
+				read(inputline, in_DATAIN); DATAIN <= to_UX01(in_DATAIN);
+				counter_data<= std_logic_vector(unsigned(counter_data)+1);
+				int_counter_data <= int_counter_data + 1;
+			else
+				done <= 1;
+			end if;
+			wait for 200 ns;
 	end process;
 	
-end tb_standard_multiplier;
+	
+			-- write result on output file
+	-- write_result_process: process is
+		-- file outputfile:			TEXT open WRITE_MODE is 
+		-- "C:\Users\lorenzo uni\Desktop\repositories\calcolatori-elettronici\standard_multiplier\result.txt";
+		-- variable inputline: 	LINE;
+	-- begin
+		-- if (DATAOUT = '1') then
+		-- -- write result
+				-- write(inputline, A);
+				-- writeline(outputfile, inputline);
+				-- write(inputline, B);
+				-- writeline(outputfile, inputline);
+				-- write(inputline, P);
+				-- writeline(outputfile, inputline);
+		-- end if;
+	-- end process;
+	
+	
+	-- terminate the simulation when there are no more data in datafile
+	done_process: process(done)		
+		variable outputline : LINE;
+	begin
+		if (done=1) then
+			write(outputline, string'("End simulation - "));
+			write(outputline, string'("cycle counter is "));
+			write(outputline, int_counter_data);
+			writeline(output, outputline);
+		assert false
+			report "NONE. End of simulation."
+			severity failure;
+		end if;
+	end process;
+	
+end behavior;

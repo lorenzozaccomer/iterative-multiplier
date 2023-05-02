1ca7af7edf6af7ac18e7c7491a26bb34e7ce2fc3
Lorenzo Zaccomer <lz.131034@gmail.com>
2023-05-02 19:20:25 +0200

    (BASIC_MULT): remove not used element in component file

--- iterative_multiplier/modules/basic_mult/components.vhd
+++ iterative_multiplier/modules/basic_mult/components.vhd
@@ -15,6 +15,7 @@ package basic_mult_components_package is
 		);
 	end component;
 	
+	
 	component leftshiftN is
 		generic(
 			N: 	integer := 4;
@@ -24,6 +25,7 @@ package basic_mult_components_package is
 			Y: out std_logic_vector(N-1 downto 0)
 		);
 	end component;
+	
 	
 	component rightshiftN is
 		generic(
@@ -35,15 +37,7 @@ package basic_mult_components_package is
 		);
 	end component;
 	
-	component adderN is
-		generic(N 	: integer := 4);
-		port(
-		  A, B	: in std_logic_vector(N-1 downto 0); 	-- operands
-		  S			: out std_logic_vector(N downto 0) 	-- final result
-		  );
-	end component;
-	
-	
+		
 	component adderNotCOut is
 		generic(N 	: integer := 4);
 		port(
@@ -51,6 +45,7 @@ package basic_mult_components_package is
 		  S		: out std_logic_vector(N-1 downto 0) 	-- final result
 		  );
 	end component;
+	
 	
 	component multiplierN is
 		generic(N 	: integer := 2);
@@ -61,23 +56,6 @@ package basic_mult_components_package is
 	end component;
 	
 	
-	component notport is
-		port (
-				A               : in  std_logic;
-				Y               : out std_logic
-			 );
-	end component;
-		
-
-	component mux is
-		port (
-		sel: 	in std_logic;
-		I0: 	in std_logic;
-		I1: 	in std_logic;
-		Y:		out std_logic
-		);
-	end component;
-
 	component mux2N is
 		generic(N 	: integer := 8);
 		port (
@@ -101,61 +79,8 @@ package basic_mult_components_package is
 		);
 	end component;
 	
-	
-	component reg is
-		port(
-		CLK:	in std_logic;
-		RST:	in std_logic;
-		LOAD:	in std_logic;
-		D : 	in std_logic;
-		Q : 	out std_logic
-		);
-	end component;
-	
+		
 end basic_mult_components_package;
-----------------------------------------------------------------------
-
-
-
-----------------------------------------------------------------------
-package constants_components_package is
-    constant Tadd           : time := 4 ns;
-    constant Tcomp          : time := 5 ns;
-    constant Tmux           : time := 2 ns;
-    constant TRco           : time := 1 ns;
-    constant TRsu           : time := 1 ns;
-    constant Tshift         : time := 0 ns;
-    constant Tlogic         : time := 0.4 ns;
-end constants_components_package;
-----------------------------------------------------------------------
-
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.constants_components_package.all;
-
--- adder with carry out and without carry in
-
-entity adderN is
-	generic(N 	: integer := 4);
-	port(
-      A, B	: in std_logic_vector(N-1 downto 0); 	-- operands
-      S			: out std_logic_vector(N downto 0) 	-- final result
-	  );
-end entity;
-
-
-architecture behavior of adderN is
-
-	signal Sum_int : std_logic_vector(N downto 0);
-
-	begin
-	Sum_int <= std_logic_vector(unsigned('0' & A) + unsigned('0' & B));
-	S <= Sum_int;
-end behavior;
 ----------------------------------------------------------------------
 
 
@@ -182,51 +107,6 @@ architecture behavior of adderNotCOut is
 end behavior;
 ----------------------------------------------------------------------
 
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use work.constants_components_package.all;
-
-
-entity notport is
-    port (
-            A               : in  std_logic;
-            Y               : out std_logic
-         );
-end notport;
-
-architecture behavior of notport is
-begin
-    Y <= not A;
-end behavior;
-----------------------------------------------------------------------
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.constants_components_package.all;
-
-
-entity mux is
-	port (
-	sel: 	in std_logic;
-	I0: 	in std_logic;
-	I1: 	in std_logic;
-	Y:		out std_logic
-	);
-end mux;
-
-architecture behavior of mux is
-begin
-	with sel select
-		Y <= 	I0 when '0', 
-				I1 when others;
-end behavior;
-----------------------------------------------------------------------
 
 ----------------------------------------------------------------------
 library ieee;
@@ -382,39 +262,6 @@ architecture behavior of regN is
 	begin
 		if(RST = '1') then
 			Q <= (others => '0');
-		elsif rising_edge(CLK) and LOAD='1' then
-			Q <= D;
-		end if;
-	end process;
-end behavior;
-
-
-----------------------------------------------------------------------
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.constants_components_package.all;
-
-
-entity reg is
-	port(
-		CLK:	in std_logic;
-		RST:	in std_logic;
-		LOAD:	in std_logic;
-		D : 	in std_logic;
-		Q : 	out std_logic
-	);
-end reg;
-
-architecture behavior of reg is
-	begin
-	process(CLK, RST)
-	begin
-		if(RST = '1') then
-			Q <= '0';
 		elsif rising_edge(CLK) and LOAD='1' then
 			Q <= D;
 		end if;

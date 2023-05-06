8f9891fb2251a27b7e0f0da54f3bc160581bfedd
Lorenzo Zaccomer <lz.131034@gmail.com>
2023-05-06 17:47:14 +0200

    (IT_MULT64): remove not used elements from components file

--- iterative_multiplier/it_mult64/components.vhd
+++ iterative_multiplier/it_mult64/components.vhd
@@ -29,25 +29,7 @@ package it_mult64_components_package is
 			Q : out std_logic_vector(N-1 downto 0)
 		);
 	end component;
-	
-	component rightshiftN is
-		generic(
-			N: 	integer := 4;
-			SH:	integer := 1);
-		port(
-			X: in std_logic_vector(N-1 downto 0);
-			Y: out std_logic_vector(N-1 downto 0)
-		);
-	end component;
-			
-	component adderNotCOut is
-		generic(N 	: integer := 4);
-		port(
-		  A, B	: in std_logic_vector(N-1 downto 0); 	-- operands
-		  S		: out std_logic_vector(N-1 downto 0) 	-- final result
-		  );
-	end component;
-	
+		
 	
 	component mux2 is
 		port (
@@ -68,44 +50,8 @@ package it_mult64_components_package is
 		);
 	end component;
 	
-	
-	component mux4N is
-		generic(N 	: integer := 8);
-		port (
-		sel: 	in std_logic_vector(1 downto 0);
-		I0: 	in std_logic_vector(N-1 downto 0);
-		I1: 	in std_logic_vector(N-1 downto 0);
-		I2: 	in std_logic_vector(N-1 downto 0);
-		I3: 	in std_logic_vector(N-1 downto 0);
-		Y:		out std_logic_vector(N-1 downto 0)
-		);
-	end component;
 end it_mult64_components_package;
 ----------------------------------------------------------------------
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-
--- adder without carries
-
-entity adderNotCOut is
-	generic(N 	: integer := 4);
-	port(
-      A, B	: in std_logic_vector(N-1 downto 0); 	-- operands
-      S		: out std_logic_vector(N-1 downto 0) 	-- final result
-	  );
-end entity;
-
-architecture behavior of adderNotCOut is
-	begin
-		S <= std_logic_vector(unsigned(A) + unsigned(B));
-end behavior;
-----------------------------------------------------------------------
-
 
 
 ----------------------------------------------------------------------
@@ -168,30 +114,6 @@ use ieee.numeric_std.all;
 
 
 
-entity rightshiftN is
-	generic(
-		N: 	integer := 4;
-		SH:	integer := 1);
-	port( 
-		X: in std_logic_vector(N-1 downto 0);
-		Y: out std_logic_vector(N-1 downto 0)
-	);
-end entity;
-
-architecture behavior of rightshiftN is
-	begin
-		Y <= std_logic_vector(shift_right(unsigned(X), SH));
-end behavior;
-----------------------------------------------------------------------
-
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-
-
 entity regN is
 	generic(N 	: integer := 4);
 	port(
@@ -216,35 +138,6 @@ end behavior;
 
 ----------------------------------------------------------------------
 
-
-----------------------------------------------------------------------
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-
-
-entity mux4N is
-	generic(N 	: integer := 8);
-	port (
-	sel: 	in std_logic_vector(1 downto 0);
-	I0: 	in std_logic_vector(N-1 downto 0);
-	I1: 	in std_logic_vector(N-1 downto 0);
-	I2: 	in std_logic_vector(N-1 downto 0);
-	I3: 	in std_logic_vector(N-1 downto 0);
-	Y:		out std_logic_vector(N-1 downto 0)
-	);
-end mux4N;
-
-architecture behavior of mux4N is
-begin
-	with sel select
-        Y <= I0 when "00",
-             I1 when "01",
-             I2 when "10",
-             I3 when others;
-end behavior;
-----------------------------------------------------------------------
 
 
 ----------------------------------------------------------------------
